310885999f2db4a7900f698e610eb31677fb1a94
cyril-musikaloid <cyril.makloufi@gmail.com>
2023-12-15 03:10:56 +0100

    trigger on js

--- src/main.cpp
+++ src/main.cpp
@@ -37,7 +37,7 @@ void setup() {
     settingator->AddSetting(Setting::Type::Slider, &red, sizeof(red), "red");
     settingator->AddSetting(Setting::Type::Slider, &green, sizeof(green), "lawngreen");
     settingator->AddSetting(Setting::Type::Slider, &blue, sizeof(blue), "blue");
-    settingator->AddSetting(Setting::Type::Switch, &trig, sizeof(trig), "Save");
+    settingator->AddSetting(Setting::Type::Trigger, nullptr, 0, "Save");
 }
 
 void loop() {
